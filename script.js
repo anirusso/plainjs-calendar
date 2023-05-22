8d63a8abc89fe4df970be45cce14d75582f43fd2
anirusso <ar.anitarusso@gmail.com>
2023-05-22 14:00:57 -0400

    upadting week display

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ months.forEach(function (item, index) {
 
 let field = document.querySelector('#selectMonth');
 field.addEventListener('change', function () {
+  const myNode = document.getElementById("weeks");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.lastChild);
+  }
   //print month & year
   var month = this.value;
   var year = new Date().getFullYear();
@@ -19,4 +23,25 @@ field.addEventListener('change', function () {
   var firstDay = new Date(date).getDay();
   //get max days in month
   var lastDay = new Date(year, months.indexOf(month)+1, 0).getDate();
+  //print calendar days
+
+  var weeks = document.getElementById("weeks");
+  day = 1;
+  for (let i = 0; i < lastDay; i++) {
+    if (i % 7 == 0) {
+      var week = document.createElement("div");
+      week.classList.add('flex-container');
+      weeks.appendChild(week);
+    }
+    var square = document.createElement("div");
+    if (day >= firstDay) {
+      square.innerHTML = day;
+    }
+    else {
+      square.innerHTML = "";
+      day++;
+    }
+    week.appendChild(square);
+  }
+
 });
